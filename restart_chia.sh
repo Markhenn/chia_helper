328654c2d3af9ba74a48b797c301ff08404e9f1d
Mark Henninger <markhenningerwork@gmail.com>
2021-05-08 18:05:33 +0200

    update restart chia to just use venv once

--- restart_chia.sh
+++ restart_chia.sh
@@ -4,11 +4,13 @@
 
 
 currentDate=$(date)
-test_case=$(cd ~/chia-blockchain && . ./activate && chia farm summary)
 
+cd ~/chia-blockchain && . ./activate
 
-if echo $test_case | grep -q 'Farming status: Not synced or not connected to peers'; then
+if chia farm summary | grep -q 'Farming status: Not synced or not connected to peers'; then
     echo "$currentDate: Restarting chia farmer" >> ~/chia_helpers/restart_chia.log
-    cd ~/chia-blockchain && . ./activate && chia start -r farmer && sleep 60s && chia show -a 45.77.67.65:8444 >> ~/chia_helpers/restart_chia.log
+    chia start -r farmer && sleep 60s && chia show -a 45.77.67.65:8444 >> ~/chia_helpers/restart_chia.log
+else
+    echo "$currentDate: No Need to restart!" >> ~/chia_helpers/restart_chia.log
 fi
 
